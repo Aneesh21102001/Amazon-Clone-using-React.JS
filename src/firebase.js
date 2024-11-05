55d2257085d193273595c382d14402e774a3083a
Aneesh <84623256+Aneesh21102001@users.noreply.github.com>
2024-11-05 10:37:17 +0530

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,24 +4,18 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyB-f63QYJ_mbHhqcbG2c7SACPOxZrogncY",
-  authDomain: "clone-5da2c.firebaseapp.com",
-  projectId: "clone-5da2c",
-  storageBucket: "clone-5da2c.appspot.com",
-  messagingSenderId: "64725500834",
-  appId: "1:64725500834:web:614767ff704272cc5ea8fb",
-  measurementId: "G-QBKHJCKYF4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-
-// Initialize Firestore
-const db = getFirestore(firebaseApp);  // Ensure this is correctly imported and set up
-
-// Initialize Auth
+const db = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
 
 export { db, auth };
